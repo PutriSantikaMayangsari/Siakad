7e0c9971f7327b34a920b37553a016307226d54d
PutriSantikaMayangsari <83493403+PutriSantikaMayangsari@users.noreply.github.com>
2022-10-19 11:53:06 +0700

    Update template.php

--- app/Views/template.php
+++ app/Views/template.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 
 <head>
@@ -30,9 +26,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="https://siakadu.unila.ac.id/assets/v1/img/logo_unila.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
           <span class="brand-text font-weight-light">Sistem Informasi Akademik</span><br>
         </a>
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
         <div class="collapse navbar-collapse order-3" id="navbarCollapse">
           <!-- Left navbar links -->
@@ -40,75 +33,91 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <li class="nav-item">
               <a href="/home" class="nav-link">Beranda</a>
             </li>
-            <li class="nav-item">
-              <a href="/about" class="nav-link">Portal</a>
+            <li class="nav-item dropdown">
+              <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Portal</a>
+              <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                <li><a href="/mahasiswa" class="dropdown-item">Mahasiswa </a></li>
+                <li><a href="/jadwal" class="dropdown-item">Jadwal </a></li>
+              </ul>
             </li>
-            <li class="nav-item">
-              <a href="/mahasiswa" class="nav-link">Mahasiswa</a>
+            <li class="nav-item dropdown">
+              <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Perkuliahan</a>
+              <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                <li><a href="/krs" class="dropdown-item">Kartu Rencana Studi </a></li>
+              </ul>
             </li>
-          </ul>
-
         </div>
-
         <!-- Right navbar links -->
+        <div class="navbar-custom-menu"></div>
         <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
-          <!-- Messages Dropdown Menu -->
+          <!-- Notifications Dropdown Menu -->
           <li class="nav-item dropdown">
-            <!-- ................... -->
+            <a class="nav-link" data-toggle="dropdown" href="#">
+              <i class="far fa-bell"></i>
+            </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+              <span class="dropdown-header">Dokumen Pendukung / UG</span>
+              <div class="dropdown-divider"></div>
               <a href="#" class="dropdown-item">
-
+                <i class="fas fa-envelope mr-2"></i> Panduan Siakad Mahasiswa
+              </a>
             </div>
           </li>
+          <!-- Notifications Dropdown Menu -->
           <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-              <i class="fas fa-th-large"></i>
+            <a class="nav-link" href="/gate">
+              <i class="fas fa-th-large"></i> Menu
             </a>
           </li>
         </ul>
       </div>
-    </nav>
-    <!-- /.navbar -->
+      </li>
+      </ul>
+  </div>
+  </nav>
+  </div>
+  <!-- /.navbar -->
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <div class="container">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0"><?= esc($title) ?></h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#"><?= esc($title) ?></a></li>
-              </ol>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
 
-      <!-- Main content -->
-      <div class="content">
-        <div class="container">
-          <?= $this->renderSection('content') ?>
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content -->
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0"><?= esc($title) ?></h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="#"><?= esc($title) ?></a></li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-    <!-- /.content-wrapper -->
+    <!-- /.content-header -->
 
-    <!-- Main Footer -->
-    <footer class="main-footer">
-      <!-- To the right -->
-      <div class="float-right d-none d-sm-inline">
-        Praktikum Web Lanjut
-      </div>
-      <!-- Default to the left -->
-      <strong>Copyright &copy; 2022 </strong> All rights reserved.
-    </footer>
+    <!-- Main content -->
+    <div class="content">
+      <div class="container">
+        <?= $this->renderSection('content') ?>
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Praktikum Web Lanjut
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 </strong> All rights reserved.
+  </footer>
   </div>
   <!-- ./wrapper -->
 
