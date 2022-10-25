1da7906a90999fd5efcf9e93079be43d16f32f6b
nabilaa30 <83420514+nabilaa30@users.noreply.github.com>
2022-10-25 21:54:19 +0700

    Add files via upload

---
+++ app/Views/pages/dosen.php
@@ -0,0 +1,156 @@
+<?= $this->extend('template') ?>
+<?= $this->section('content') ?>
+
+<section class="terms-condition ptb-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <div class="content-with-sidebar">
+                    <div class="card single-pricing-pack">
+                        <div class="card-header text-center ">
+                            <h5 class="mb-0">Profil Dosen </h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                <div class="row">
+
+                                    <div class="col-md-9">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>NIP</strong>
+                                            </div>
+                                            <div class="col-sm-5">
+
+                                                197311122000031003
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>Nama</strong>
+                                            </div>
+                                            <div class="col-sm-5">
+                                                Muhammad Ridho, M.Kom </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>Jenis Kelamin</strong>
+                                            </div>
+                                            <div class="col-sm-5">
+                                                LAKI-LAKI </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>Unit Kerja</strong>
+
+                                            </div>
+                                            <div class="col-sm-5">
+
+                                                Program Studi S1 Ilmu Komputer
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>Jabatan Terakhir </strong>
+
+                                            </div>
+                                            <div class="col-sm-5">
+
+                                                Lektor/Ketua Program Studi S1 Ilmu Komputer
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong> Pangkat Gol/Ruang </strong>
+
+                                            </div>
+                                            <div class="col-sm-5">
+                                                III/d, Penata Tk. I,
+
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>Status Pegawai</strong>
+
+                                            </div>
+                                            <div class="col-sm-5">
+                                                PNS
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <strong>Status Aktif Pegawai</strong>
+
+                                            </div>
+                                            <div class="col-sm-5">
+                                                Aktif
+                                            </div>
+                                        </div>
+                                        <div class="row pt-20">
+
+                                        </div>
+                                    </div>
+                                    <!-- batas data pribadi -->
+
+                                    <div class="col-md-3">
+                                        <img src="http://siladas.radenfatah.ac.id/assets/img/laki-laki.jpg" class="img-thumbnail" alt="Image">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="card single-pricing-pack">
+                        <div class="card-header text-center ">
+                            <h5 class="mb-0">Riwayat Pendidikan Formal</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Jenjang Pendidikan</th>
+                                            <th>Nama Instansi Pendidikan</th>
+                                            <th>Jurusan</th>
+                                            <th>Tahun Lulus</th>
+                                            <th>Tempat</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>SD</td>
+                                            <td>Al-Kautsar Bandar Lampung</td>
+                                            <td>-</td>
+                                            <td>1987</td>
+                                            <td>Bandar Lampung</td>
+                                        </tr>
+                                        <tr>
+                                            <td>S1</td>
+                                            <td>Universitas Pendidikan Indonesia</td>
+                                            <td>Ilmu Komputer</td>
+                                            <td>1999</td>
+                                            <td>Bandung</td>
+                                        </tr>
+                                        <tr>
+                                            <td>S2</td>
+                                            <td>Universitas Indonesia</td>
+                                            <td>Ilmu Komputer</td>
+                                            <td>2007</td>
+                                            <td>Jakarta</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <?= $this->endSection() ?>
