3d8624f7e9179d5c8ad7e95df2673c42082fb0f9
nabilaa30 <83420514+nabilaa30@users.noreply.github.com>
2022-11-01 23:21:34 +0700

    update views mahasiswa (Dosen)

--- app/Views/mahasiswa/edit.php
+++ app/Views/mahasiswa/edit.php
@@ -1,28 +1,24 @@
 <?= $this->extend('template') ?>
 <?= $this->section('content') ?>
 <div class="p-4">
-    <form action="/update/<?= $id ?>" method="post">
-        <div class="row">
-            <div class="col-6">
-                <div class="mb-3">
-                    <label for="npm" class="form-label">NPM</label>
-                    <input type="text" name="npm" class="form-control" id="npm" value="<?= $npm ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="nama" class="form-label">Nama</label>
-                    <input type="text" name="nama" class="form-control" id="nama" value="<?= $nama ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="alamat" class="form-label">Alamat</label>
-                    <input type="text" name="alamat" class="form-control" id="alamat" value="<?= $alamat ?>">
-                </div>
-            </div>
-            <div class="col-6">
-                <label for="deskripsi" class="form-label">Deskripsi</label>
-                <textarea name="deskripsi" id="deskripsi" cols="30" rows="10" class="form-control"><?= $deskripsi ?></textarea>
-            </div>
+    <form class="mx-5" action="/update/<?= $mahasiswa['id'] ?>" method="post">
+        <div class="form-group">
+            <label for="npm">NPM Mahasiswa</label>
+            <input type="number" class="form-control" id="npm" name="npm" value="<?= $mahasiswa['npm'] ?>">
+        </div>
+        <div class="form-group">
+            <label for="nama">Nama Mahasiswa</label>
+            <input type="text" class="form-control" id="nama" name="nama" value="<?= $mahasiswa['nama']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="alamat">Alamat Mahasiswa</label>
+            <textarea type="text" class=" form-control" id="alamat" rows="3" name="alamat"><?= $mahasiswa['alamat']; ?></textarea>
+        </div>
+        <div class="form-group">
+            <label for="deskripsi" class="form-label">Deskripsi Mahasiswa</label>
+            <textarea type="text" name="deskripsi" class="form-control" id="deskripsi"><?= $mahasiswa['deskripsi'] ?></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-    <div>
-        <?= $this->endSection() ?>
+
+    <?= $this->endSection() ?>
