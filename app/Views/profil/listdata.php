22506059a898add36cab9b802a97c79deb6c8847
PutriSantikaMayangsari <83493403+PutriSantikaMayangsari@users.noreply.github.com>
2022-11-13 21:39:58 +0700

    Update listdata.php

--- app/Views/profil/listdata.php
+++ app/Views/profil/listdata.php
@@ -1,73 +1,135 @@
 <?= $this->extend('template') ?>
 <?= $this->section('content') ?>
+
 <div class="col-lg-12">
     <div class="card">
-        <div class="card-header">
-            <form method="post" id="form_filter">
-                <div class="panel-body">
-                    <br>
-                    <a href="/createdata" type="button" class="btn btn-primary mb-3"><i class="fa fa-plus"></i> Tambah</a>
-                    <a href="/editdata" type="button" class="btn btn-warning mb-3"><i class="fa fa-edit"></i> Edit</a>
-                </div>
-            </form>
+        <div class="container">
+            <div class="navbar-custom-menu"></div>
+            <br>
+            <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+                <?php $no = 1;
+                foreach ($profil as $dm) : ?>
+                    <div class="d-flex">
+                        <a class="btn btn-warning mr-3" href="/editprofil/<?= $dm['id_profil'] ?>"><i class="fa fa-edit"></i> Edit</a>
+                    </div>
+                <?php $no++;
+                endforeach; ?>
+            </ul>
         </div>
+        <br>
 
-        <!-- Main row -->
-        <div class="row">
-            <!-- Left col -->
-            <section class="col-lg-12 connectedSortable">
-                <!-- Custom tabs (Charts with tabs)-->
-                <table class="table table-bordered table-striped dataTable">
-                    <thead>
-                        <tr>
-                            <th scope="col">NPM</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Prodi</th>
-                            <th scope="col">Jalur</th>
-                            <th scope="col">Jenis Kelamin</th>
-                            <th scope="col">tanggal lahir</th>
-                            <th scope="col">tempat lahir</th>
-                            <th scope="col">gol.darah</th>
-                            <th scope="col">nama ayah</th>
-                            <th scope="col">nama ibu</th>
-                            <th scope="col">Penghasilan ayah</th>
-                            <th scope="col">Penghasilan ibu</th>
-                            <th scope="col">Pendidikan</th>
-                            <th scope="col">Alamat Rumah</th>
-                            <th scope="col">Kewarganegaraan</th>
-                            <th scope="col">Kode Post</th>
-                            <th scope="col">Status Tinggal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $no = 1;
-                        foreach ($profil as $dm) : ?>
-                            <tr>
-                                <td><?= $dm['npm'] ?></td>
-                                <td><?= $dm['nama'] ?></td>
-                                <td><?= $dm['prodi'] ?></td>
-                                <td><?= $dm['jalur'] ?></td>
-                                <td><?= $dm['jk'] ?></td>
-                                <td><?= $dm['tlahir'] ?></td>
-                                <td><?= $dm['tmptlahir'] ?></td>
-                                <td><?= $dm['goldar'] ?></td>
-                                <td><?= $dm['ayah'] ?></td>
-                                <td><?= $dm['ibu'] ?></td>
-                                <td><?= $dm['penghasilanayah'] ?></td>
-                                <td><?= $dm['penghasilanibu'] ?></td>
-                                <td><?= $dm['pendidikan'] ?></td>
-                                <td><?= $dm['alamat'] ?></td>
-                                <td><?= $dm['kewarganegaraan'] ?></td>
-                                <td><?= $dm['kode'] ?></td>
-                                <td><?= $dm['status'] ?></td>
-                            </tr>
-                        <?php $no++;
-                        endforeach; ?>
-                    </tbody>
-                </table>
-            </section>
+        <div class="col-md-16">
+            <div class="card card-primary card-tabs">
+                <div class="card-header p-0 pt-1">
+                    <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="custom-tabs-one-berita-tab" data-toggle="tab" href="#custom-tabs-one-berita" role="tab" aria-controls="custom-tabs-one-berita" aria-selected="true">Informasi Umum</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="custom-tabs-one-vm-tab" data-toggle="tab" href="#custom-tabs-one-vm" role="tab" aria-controls="custom-tabs-one-vm" aria-selected="false">Domisili</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="custom-tabs-one-dm-tab" data-toggle="tab" href="#custom-tabs-one-dm" role="tab" aria-controls="custom-tabs-one-dm" aria-selected="false">Orangtua</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content" id="custom-tabs-one-tabContent">
+                        <div class="tab-pane fade show active" id="custom-tabs-one-berita" role="tabpanel" aria-labelledby="custom-tabs-one-berita-tab">
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table class="table table-bordered table-striped dataTable">
+                                    <thead>
+                                        <?php $no = 1;
+                                        foreach ($profil as $dm) : ?>
+                                            <tr>
+                                                <th scope="col">Npm</th>
+                                                <td><?= $dm['npm'] ?></td>
+                                                <th scope="col">Nama</th>
+                                                <td><?= $dm['nama'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['prodi'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['jalur'] ?></td>
+                                            </tr>
+                                        <?php $no++;
+                                        endforeach; ?>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="custom-tabs-one-vm" role="tabpanel" aria-labelledby="custom-tabs-one-vm-tab">
+                            <div class="card-body">
+                                <table class="table table-bordered table-striped dataTable">
+                                    <thead>
+                                        <?php $no = 1;
+                                        foreach ($profil as $dm) : ?>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['jk'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['tlahir'] ?></td>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['tmptlahir'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['goldar'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['kewarganegaraan'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['kode'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['status'] ?></td>
+                                            </tr>
+                                        <?php $no++;
+                                        endforeach; ?>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="custom-tabs-one-dm" role="tabpanel" aria-labelledby="custom-tabs-one-dm-tab">
+                            <div class="card-body">
+                                <table class="table table-bordered table-striped dataTable">
+                                    <thead>
+                                        <?php $no = 1;
+                                        foreach ($profil as $dm) : ?>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['ayah'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['ibu'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['penghasilanayah'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['penghasilanibu'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['pendidikan'] ?></td>
+                                                <th scope="col"></th>
+                                                <td><?= $dm['alamat'] ?></td>
+                                            </tr>
+                                        <?php $no++;
+                                        endforeach; ?>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+            </div>
         </div>
     </div>
-</div>
-<div>
     <?= $this->endSection() ?>
